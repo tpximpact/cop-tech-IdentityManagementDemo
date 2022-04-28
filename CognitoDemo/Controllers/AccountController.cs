5273cfa4cb229d242c31bb63dfcca7fe15e5b7fe
Matt Dendle <matt.dendle@foundry4.com>
2022-04-28 20:58:13 +0100

    Initial Cognito Project

---
+++ CognitoDemo/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CognitoDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        public void Login(string returnUrl = "/")
+        {
+            
+        }
+
+        [Authorize]
+        public async Task Logout()
+        {
+          
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+    }
+}
---
+++ CognitoDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CognitoDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
